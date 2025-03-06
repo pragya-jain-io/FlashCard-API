623e7f8035ad2cce5903fbb2b64b3370039fa441
Pragya Jain <123pragyajain122@gmail.com>
2025-03-06 17:22:07 +0530

    Initial project setup
    # Conflicts:
    #	.gitignore

---
+++ src/test/kotlin/com/pragya/flashcard/FlashCardApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pragya.flashcard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FlashCardApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
