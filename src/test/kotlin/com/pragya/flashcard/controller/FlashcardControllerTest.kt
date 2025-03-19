be250da8a4921d9a1b4658b50d9364209b0c8961
Pragya Jain <123pragyajain122@gmail.com>
2025-03-19 16:02:49 +0530

    refactor: controller test for readability
    # Conflicts:
    #	.gitignore

--- src/test/kotlin/com/pragya/flashcard/controller/FlashcardControllerTest.kt
+++ src/test/kotlin/com/pragya/flashcard/controller/FlashcardControllerTest.kt
@@ -3,10 +3,9 @@ package com.pragya.flashcard.controller
 import com.pragya.flashcard.model.Flashcard
 import com.pragya.flashcard.repository.FlashcardRepository
 import com.pragya.flashcard.service.FlashcardService
-
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
 import org.springframework.beans.factory.annotation.Autowired
@@ -19,43 +18,38 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.reactive.server.WebTestClient
 import reactor.core.publisher.Mono
 
-import kotlin.test.Test
-
 @WebFluxTest(FlashcardController::class)
 @ExtendWith(SpringExtension::class)
 @Import(FlashcardService::class)
-@ActiveProfiles("test")
+//@ActiveProfiles("test")
 class FlashcardControllerTest @Autowired constructor(
-    private val webTestClient: WebTestClient,
-//    private val repository: FlashcardRepository
-)  {
+    private val webTestClient: WebTestClient
+) {
+
     @MockBean
     private lateinit var repository: FlashcardRepository
 
     @BeforeEach
     fun setup() {
-//        repository.deleteAll().block()
         MockitoAnnotations.openMocks(this)
     }
 
     @Test
-    fun createFlashcard() {
-        val request = Flashcard(
+    fun flashcardCreatedSuccess() {
+        val flashcard = Flashcard(
             question = "What is Spring Boot?",
             answer = "A framework for building Java applications",
             tags = listOf("Spring", "Java")
         )
-//        Mockito.`when`(repository.save(any())).thenReturn(Mono.just(request))
-        Mockito.`when`(repository.save(any<Flashcard>())).thenReturn(Mono.just(request))
+
+        Mockito.`when`(repository.save(Mockito.any())).thenReturn(Mono.just(flashcard))
+
         webTestClient.post().uri("/api/flashcards")
             .contentType(MediaType.APPLICATION_JSON)
-            .bodyValue(request)
+            .bodyValue(flashcard)
             .exchange()
             .expectStatus().isOk
             .expectBody()
-            .jsonPath("$.question").isEqualTo("What is Spring Boot?")
+            .jsonPath("$.question").isEqualTo(flashcard.question)
     }
-
-
-
 }
